ff84edc075835d4ce307cdd3d0b984e2d8a08dc6
Jordan Deren <jordanderen1@gmail.com>
2021-02-20 19:20:42 +0000

    do

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,2 +1,9 @@
 module CocktailsHelper
+  def cocktail_image(cocktail)
+    if cocktail.photo.attached?
+    cl_image_path(cocktail.photo.key)
+  else
+    asset_path('cocktail_default.png')
+  end
+end
 end

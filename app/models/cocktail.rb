05bc153dedb1b9ccaa4301b1b2b487a3918b3279
Jordan Deren <jordanderen1@gmail.com>
2021-02-20 18:42:09 +0000

    f

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,7 +1,8 @@
 class Cocktail < ApplicationRecord
     has_many :doses, dependent: :destroy
     has_many :ingredients, through: :doses
-   validates :name, uniqueness: true, presence: true
+    validates :name, uniqueness: true, presence: true
 
+    has_one_attached :photo
 
 end
